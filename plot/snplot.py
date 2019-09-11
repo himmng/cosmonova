b73131de31a4b98c63bfdd83cd74a45a9f38d8ba
himmng <himanshuhimang@gmail.com>
2019-09-11 21:55:40 +0530

    added corner plot

--- plot/snplot.py
+++ plot/snplot.py
@@ -7,21 +7,29 @@ omega_m , h = np.loadtxt(path+"cosmonovaIa.out", usecols = (0,1), unpack = True)
 
 logL = np.loadtxt(path+"cosmonovaIaprob.out", usecols = (0), unpack = True) #=== loglikelyhood =====#
 
-
+plt.figure(figsize=(10,10))
+plt.suptitle("hubble paramater $h$ vs. matter density parameter $\Omega_m$ ",size = 15)
 plt.subplot(221)
 plt.ylabel('freq.')
 plt.xlabel('$h$')
 
-plt.hist(h,bins = 100)
+plt.hist(h,bins = 100,color='purple')
 
 plt.subplot(222)
 plt.ylabel('freq.')
 plt.xlabel('$\Omega_m$')
-plt.hist(omega_m,bins=100)
+plt.hist(omega_m,bins=100,color='orange')
 
-plt.subplot(223)
-#plt.title("scatter plot: $h$ vs. $\Omega_m$ ",size = 8)
+plt.subplot(212)
+
 plt.xlabel('$\Omega_m$')
 plt.ylabel('$h$')
-plt.scatter(omega_m , h, c = -logL)
-plt.savefig('params.png')
+plt.scatter(omega_m , h, c = -logL,cmap='cividis_r')
+
+plt.savefig('hist_&_scatter.png')
+
+a=[]
+a = np.c_[h,omega_m]
+fig = corner.corner(a,bins=100,color='b',weights = -logL,labels = ['$h$','$\Omega_m$'])
+fig.suptitle("hubble paramater $h$ vs. matter density parameter $\Omega_m$ ")
+fig.savefig('corner_plot.png')
