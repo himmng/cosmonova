030484a8651dc3c726c5acd0cb7aa43084067022
himmng <himanshuhimang@gmail.com>
2019-09-11 20:00:20 +0530

    moved cosmonovasampler to supernvaIa

--- supernovaIa/cosmonovasampler.py
+++ supernovaIa/cosmonovasampler.py
@@ -1,11 +1,10 @@
 from cosmoHammer import MpiCosmoHammerSampler
 from cosmoHammer import LikelihoodComputationChain
 from cosmoHammer.util import Params
-from supernovaIa.supernovaIacore import supercore
-from supernovaIa.supernovaIalike import snlikeModule as slk
-
-params = Params(("Omega_m",[0.18,0.18,0.42,0.01]),
-                ("h", [0.60,0.60,0.80,0.01]))
+from supernovaIalike import snlikeModule as slk
+from supernovaIacore import supercore
+params = Params(("Omega_m",[0.31,0.18,0.42,0.01]),
+                ("h", [0.70,0.60,0.80,0.01]))
 chain = LikelihoodComputationChain(min = params[:,1] , max = params[:,2])
 
 chain.addCoreModule(supercore())
--- supernovaIa/snData.py
+++ supernovaIa/snData.py
@@ -8,9 +8,9 @@ dbins=31 #--------------no. of bins given in the data file--------#
 sigma=0.01 #-------assigning sigma for both parameters---------#
 
 # reading the data file consisting of redshifts and distance modulus
+path = '/home/ht/PycharmProjects/cosmonova/supernova_data'
 
-
-z,mu_ob=np.loadtxt('jla_mub_0.txt',usecols=(0,1),unpack=True)
+z,mu_ob=np.loadtxt(path+'/jla_mub_0.txt',usecols=(0,1),unpack=True)
 
 
 
@@ -18,7 +18,7 @@ z,mu_ob=np.loadtxt('jla_mub_0.txt',usecols=(0,1),unpack=True)
 # reading & reshaping covarience matrix  to  31*31 matrix
 
 
-cov=np.loadtxt('jla_mub_covmatrix.txt',unpack=True)
+cov=np.loadtxt(path+'/jla_mub_covmatrix.txt',unpack=True)
 cov_err=cov.reshape((31,31))
 #cov_inv=np.zeros(shape=(31,31))
 
